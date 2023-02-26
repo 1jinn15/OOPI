87f455351bafd6dfb2367abbcf05f074aeca3caa
1jinn15 <62107953+1jinn15@users.noreply.github.com>
2023-02-27 00:10:03 +0800

    Update A1083323_0224_2.java

--- A1083323_0224_2.java
+++ A1083323_0224_2.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class A1083323_0224_2{
 	public static void main(String[] args){
 		System.out.println("plz enter a num:");
-        Scanner sc = new Scanner(System.in);
-        int cel = sc.nextInt();
-        int fah = (cel*9/5)+32;
-        System.out.println("its-> "+cel+" degrees Celsius and "+fah+" degrees Fahrenheit");
+        	Scanner sc = new Scanner(System.in);
+        	int cel = sc.nextInt();
+        	int fah = (cel*9/5)+32;
+        	System.out.println("its-> "+cel+" degrees Celsius and "+fah+" degrees Fahrenheit");
 	}
 
 }
