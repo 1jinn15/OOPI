e8342d56cc608a9417d6c87d4ef27bf07cc98c2d
1jinn15 <62107953+1jinn15@users.noreply.github.com>
2023-02-27 00:08:56 +0800

    Add files via upload
    2/24作業

---
+++ A1083323_0224_1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class A1083323_0224_1{
+	public static void main(String[] args){
+		System.out.println("plz enter a num:");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if(num%2==0){
+            System.out.println("it is even");
+        }else{
+            System.out.println("its odd");
+        }
+	}
+
+}
